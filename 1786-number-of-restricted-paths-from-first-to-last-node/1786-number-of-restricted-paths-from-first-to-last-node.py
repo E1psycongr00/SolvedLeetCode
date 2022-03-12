d0f5adf308240c071dddfcdabb44c904cc374350
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-03-12 19:00:10 +0900

    Time: 3909 ms (17.39%), Space: 78 MB (10.32%) - LeetHub

--- 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.py
+++ 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.py
@@ -1,40 +1,42 @@
-import collections
-import heapq
+from collections import defaultdict
+from heapq import *
 from functools import lru_cache
+import sys
 
 class Solution:
-    def countRestrictedPaths(self, n, edges):
-
+    def getRestrictedPath(self, source, n ,G):
+        # Dijstra
+        INF = sys.maxsize
+        dist = [INF] * (n+1)
+        minHeap = [(0, source)]
+        while minHeap:
+            cost, node = heappop(minHeap)
+            if dist[node] != INF : continue
+            dist[node] = cost
+            for nextCost, nextNode in G[node]:
+                if dist[nextNode] == INF:
+                    alt = (cost + nextCost)
+                    heappush(minHeap, (cost+nextCost, nextNode))
+        return dist
+    
+    
+    def countRestrictedPaths(self, n: int, edges: List[List[int]]) -> int:     
         @lru_cache(maxsize=2 * (10**4))
-        def path(u):
-            if u == n:
+        def dfs(node):
+            if node == n:
                 return 1
-            ans = 0
-            for v, w in g[u].items():
-                if dis[v] >= dis[u]:
-                    continue
-                ans += path(v)
-            return ans
-
-        # 1, build graph
-        g = [collections.defaultdict(lambda: float('inf')) for _ in range(n+1)]
-        for u, v, w in edges:
-            g[u][v] = w
-            g[v][u] = w
-			
-        # 2. foreach node, find out the shortest path toward n
-        dis = [float('inf') for _ in range(n+1)]
-        g[n][n] = 0
+            ret = 0
+            for _, nextNode in G[node]:
+                if dist[node] > dist[nextNode]:
+                    ret += dfs(nextNode)
+            return ret
         
-		# (node, w)
-        queue = [(0, n)]
-        heapq.heapify(queue)
-        while queue:
-            d, u = heapq.heappop(queue)
-            if dis[u]  != float('inf'):continue
-            dis[u] = d
-            for v, w in g[u].items():
-			    # find the shortest path
-                if dis[v] == float('inf'):
-                    heapq.heappush(queue, (d+w, v))
-        return path(1) % (10**9 + 7)
+        G = defaultdict(list)
+        
+        for u, v, w in edges:
+            G[u].append((w,v))
+            G[v].append((w,u))
+        
+        dist = self.getRestrictedPath(n, n, G)
+        ret = dfs(1) % (10**9 + 7)
+        return ret
