58df226f9829cd780395cffc9882497eafdd980f
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-04-01 15:07:04 +0900

    Time: 486 ms (5.14%), Space: 16.7 MB (22.37%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.py
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -1,26 +1,49 @@
+class UnionFind:
+    parent = []
+    rank = []
+    virtualNode:int
+    def __init__(self, rows, cols):
+        self.parent = [i for i in range(rows * cols + 1)]
+        self.rank = [1 for _ in range(rows * cols + 1)]
+        self.virtualNode = rows * cols
+        
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def merge(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x != y: self.parent[y] = x
+    
+    def isConnected(self,x, y):
+        return self.find(x) == self.find(y)
+    
+
 class Solution:
-    def solve(self, board: List[List[str]]) -> None:
+    def solve(self, board) -> None:
         rows, cols = len(board), len(board[0])
-        def capture(r, c):
-            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != 'O':
-                return
-            board[r][c] = 'T'
-            capture(r+1, c)
-            capture(r-1, c)
-            capture(r, c+1)
-            capture(r, c-1)
+        uf = UnionFind(rows, cols)
+        
+        def index(cols, y, x):
+            return y * cols + x
+        
         
         for r in range(rows):
             for c in range(cols):
                 if board[r][c] == 'O' and (r in [0, rows-1] or c in [0, cols-1]):
-                    capture(r, c)
-                    
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c] == 'O':
-                    board[r][c] = 'X'
-                    
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c] == 'T':
-                    board[r][c] = 'O'
+                    uf.merge(uf.virtualNode, index(cols, r, c))
+                
+                elif board[r][c] == 'O':
+                    curNode = index(cols, r, c)
+                    if r + 1 < rows and board[r + 1][c] == 'O': uf.merge(curNode, index(cols, r + 1, c))
+                    if r - 1 >= 0 and board[r - 1][c] == 'O': uf.merge(curNode, index(cols, r - 1, c))
+                    if c + 1 < cols and board[r][c + 1] == 'O': uf.merge(curNode, index(cols, r, c + 1))
+                    if c - 1 >= 0 and board[r][c - 1] == 'O': uf.merge(curNode, index(cols, r, c - 1))
+        
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == 'O' and not uf.isConnected(index(cols, i, j), uf.virtualNode):
+                    board[i][j] = 'X'
+        
